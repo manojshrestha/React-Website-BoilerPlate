e1841a2018ab566881e1ea001d77de1bc9abf60d
manojshrestha <iam.manojshrestha@gmail.com>
2021-03-14 00:47:08 +0900

    Header section added.

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -41,20 +41,6 @@
               <li className="nav-item">
                 <a className="nav-link" href="#Travel">Travel</a>
               </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#Contact">Contact</a>
-              </li>
-              {/* <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Dropdown
-                </a>
-                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <a className="dropdown-item" href="#">Action</a>
-                  <a className="dropdown-item" href="#">Another action</a>
-                  <div className="dropdown-divider"></div>
-                  <a className="dropdown-item" href="#">Something else here</a>
-                </div>
-              </li> */}
             </ul>
           </div>
           </div>
